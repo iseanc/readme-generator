9ff12d8e9dab2e9000a9b20a71bfef8e9091bd7c
Sean Collins <iseanc@hotmail.com>
2022-11-09 15:44:31 -0700

    Commit locally initialized repo. It has base files, plus in created package.json, installed npm inquirer@8.2.4, created a designNotes file with requirements, etc, and started editing index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// VARIABLES:
+let licenseBadge, title, description, toc, installation, usage, license, contributing, tests, githubUsername, githubProfileURL, emailAddress;
+
+// TODO: Create an array of questions for user input
+const arrayQuestions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
