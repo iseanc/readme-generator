450f4de139edcc6259f0da7c138be7b97b761329
Sean Collins <iseanc@hotmail.com>
2022-11-12 18:45:52 -0700

    all JavaScript done except for Table Of Contents functionality

---
+++ data/README.md
@@ -0,0 +1,29 @@
+# A
+
+## Description
+
+B
+
+## Table of Contents
+
+## Installation
+
+C
+
+## Usage
+
+D
+
+## Contributing
+
+F
+
+## Tests
+
+G
+
+## Questions
+
+Visit me [on Github](https://github.com/A).
+You may also email me with questions and comments at B
+
--- index.js
+++ index.js
@@ -1,76 +1,94 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const gm = require('./utils/generateMarkdown');
 
-// VARIABLES:
-let licenseBadge, title, description, toc, installation, usage, license, contributing, tests, githubUsername, githubProfileURL, emailAddress;
+// Array of SPDX license IDs (for opensource.org)
+const licenses = [
+  'NONE',
+  'Unlicense', 
+  'BSL-1.0', 
+  'MIT', 
+  'Apache-2.0', 
+  'MPL-2.0', 
+  'LGPL-3.0', 
+  'GPL-3.0', 
+  'AGPL-3.0'
+];
 
 // TODO: Create an array of questions for user input
-// const arrayQuestions = [];
-inquirer
-  .prompt([
+const arrayQuestions = [
     {
       type: 'input',
-      message: 'What is the title of your project?  ',
+      message: 'What is the title of this project? ',
       name: 'title',
     },
     {
       type: 'input',
-      message: 'Provide a short description.  Think about your motivation, what problem it solves, why you built it, and what you learned:  ',
+      message: 'Project description: What problem is solved? What was the motivation? ',
       name: 'description',
     },
     {
       type: 'input',
-      message: 'What are the steps required to install your project?  ',
+      message: 'What steps are required to install your project? (leave blank to skip)',
       name: 'installation',
     },
     {
       type: 'input',
-      message: 'List any usage instructions and examples for use:  ',
+      message: 'List usage instructions and examples for use: (leave blank to skip)',
       name: 'usage',
     },
     {
       type: 'list',
-      message: 'What kind of license will this project use?  ',
-      name: 'contact',
-      choices: ['MIT License', 'GNU GPLv3', 'The Unlicense'],
+      message: 'What kind of license will this project use? ',
+      name: 'license',
+      // https://choosealicense.com/licenses/
+      choices: licenses
     },
     {
       type: 'input',
-      message: 'Provide information on how to contributing to your project:  ',
+      message: 'How to contribute to the project: (leave blank to skip) ',
       name: 'contributing',
     },
     {
       type: 'input',
-      message: 'Provide examples of how to run any tests here:  ',
+      message: 'Examples of how to run any tests: (leave blank to skip) ',
       name: 'tests',
     },
     {
       type: 'input',
-      message: 'Enter your GitHub user name:  ',
+      message: 'Enter your GitHub user name: (leave blank to skip)',
       name: 'githubUsername',
     },
     {
       type: 'input',
-      message: 'Enter the URL for your GitHub profile:  ',
-      name: 'githubProfileURL',
-    },
-    {
-      type: 'input',
-      message: 'Enter your email address:  ',
+      message: 'Enter your email address: (leave blank to skip)',
       name: 'emailAddress',
     }
-  ])
-  .then((response) =>
-    console.log(response)
-  );
-
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+
+  fs.writeFile('./data/README.md', data, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(arrayQuestions)
+  .then((response) => { 
+    // writeToFile(gm(response));
+    console.log(gm(response));
+    }
+  );
+}
 
 // Function call to initialize app
 init();
